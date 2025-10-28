c2cde03560c6b27f0529d8a2140c0dec5e49d34c
ryan <ryantepedino37@gmail.com>
2025-10-28 13:40:22 -0300

    trocado para modelo gemini-1.5-flash estável

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.post("/ocr", async (req, res) => {
     const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${GEMINI_API_KEY}`;
 
 
+
     const bodyGemini = {
       contents: [
         {
